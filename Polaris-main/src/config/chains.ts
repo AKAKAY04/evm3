d3149c20e9e345f6fc1d4074c62e52899741c1fa
AKAKAY04 <151957946+AKAKAY04@users.noreply.github.com>
2023-12-10 02:26:11 +0800

    Add files via upload

---
+++ Polaris-main/src/config/chains.ts
@@ -0,0 +1,71 @@
+import { defineChain } from "viem";
+import {
+  arbitrum,
+  avalanche,
+  base,
+  bsc,
+  celo,
+  classic,
+  confluxESpace,
+  eos,
+  fantom,
+  filecoin,
+  gnosis,
+  linea,
+  mainnet,
+  neonMainnet,
+  okc,
+  opBNB,
+  optimism,
+  polygon,
+  sepolia,
+  zkSync,
+} from "viem/chains";
+
+export const shibarium = defineChain({
+  id: 109,
+  name: "Shibarium",
+  network: "shibarium",
+  nativeCurrency: {
+    decimals: 18,
+    name: "BONE",
+    symbol: "BONE",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.shibrpc.com"],
+    },
+    public: {
+      http: ["https://rpc.shibrpc.com"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://shibariumscan.io" },
+  },
+});
+
+export const inscriptionChains = {
+  eth: mainnet,
+  bsc,
+  opBNB,
+  okc,
+  polygon,
+  fantom,
+  avalanche,
+  arbitrum,
+  optimism,
+  base,
+  zkSync,
+  classic,
+  eos,
+  neonMainnet,
+  linea,
+  celo,
+  confluxESpace,
+  gnosis,
+  filecoin,
+  shibarium,
+  sepolia,
+};
+
+export type ChainKey = keyof typeof inscriptionChains;
